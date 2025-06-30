bc88f75a8f33b1221004d7389a9804f76c4ab83b
Ankush Kailas Dhavale <dhavaleankush7@gmail.com>
2025-06-30 17:22:09 +0530

    build basic calculator and leap year program

--- day1/script.js
+++ day1/script.js
@@ -1,6 +1,8 @@
+////// level 0
+
 console.log("hello world");
 let num = 10;
-const num2 = 'a';
+const num2 = "a";
 console.log(num2 + num);
 
 let a = 10;
@@ -19,14 +21,11 @@ console.log(Math.max(100, 577)); //577
 //min value
 console.log(Math.min(200, 7)); //7
 
-
 //ceil value
 console.log(Math.ceil(3.555)); //4
 
-
 //floor value
 console.log(Math.floor(6.66)); //6
-
 
 //round value  0.51 Above value console
 console.log(Math.round(7.666)); //8
@@ -37,42 +36,65 @@ console.log(Math.round(7.444)); //7
 //random value
 console.log(Math.random()); //0 to 1 random value generated
 
-
 //String operations
-
 
 let str = "Ankush";
 
 //uppercase
 console.log(str.toUpperCase());
 
-
 //lowercase
 console.log(str.toLowerCase());
-
 
 //indexof
 console.log(str.indexOf("u")); // 3
 
-
 //lastindexof
 console.log(str.lastIndexOf("A")); // 0 last return
-
 
 //chartAt
 console.log(str.charAt(4)); // s
 
-
 //split all value
-console.log(str.split('')); // ['A','n','k','u','s','h']
-
+console.log(str.split("")); // ['A','n','k','u','s','h']
 
 //concate
-console.log(str.concat(" Dhavale"));  // Ankush Dhavale
-
+console.log(str.concat(" Dhavale")); // Ankush Dhavale
 
 //uppercase
-console.log(str.slice(1,3)); // return 1 index to 3-1 index // nk
+console.log(str.slice(1, 3)); // return 1 index to 3-1 index // nk
 
+/// level 1
 
+// leap year
 
+let leapYear = 2028; // 2024 2020 2028
+
+if ((leapYear % 400 == 0 && leapYear % 100 != 0) || leapYear % 4 == 0)
+	console.log(`Leap Year is : ${leapYear}`);
+
+// bsics calculater
+
+let choice = parseInt(
+	prompt("Enter Choice 1:Addition 2:Subtraction 3: Multiplication 4:Division")
+);
+let number = parseInt(prompt("Enter First Number"));
+let number2 = parseInt(prompt("Enter Second Number"));
+console.log(choice);
+
+switch (choice) {
+	case 1:
+		console.log(`Addition of the two number is ${number + number2}`);
+		break;
+	case 2:
+		console.log(`Subtraction of the two number is ${number - number2}`);
+		break;
+	case 3:
+		console.log(`Multiplication of the two number is ${number * number2}`);
+		break;
+	case 4:
+		console.log(`Division of the two number is ${number / number2}`);
+		break;
+	default:
+		console.log("Please enter valid Choice");
+}
