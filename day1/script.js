da1c6ae12bc5d379409f827ac6853385798eebc9
Ankush Kailas Dhavale <dhavaleankush7@gmail.com>
2025-06-30 16:54:40 +0530

    Learning Advanced JavaScript Concept

---
+++ day1/script.js
@@ -0,0 +1,78 @@
+console.log("hello world");
+let num = 10;
+const num2 = 'a';
+console.log(num2 + num);
+
+let a = 10;
+let b = 20;
+//Math Functions
+console.log(Math.abs(12.78));
+//square root
+console.log(Math.sqrt(36)); //6
+
+//cube root
+console.log(Math.cbrt(27)); // 3
+
+//max value
+console.log(Math.max(100, 577)); //577
+
+//min value
+console.log(Math.min(200, 7)); //7
+
+
+//ceil value
+console.log(Math.ceil(3.555)); //4
+
+
+//floor value
+console.log(Math.floor(6.66)); //6
+
+
+//round value  0.51 Above value console
+console.log(Math.round(7.666)); //8
+
+//round value   0.49 below value console
+console.log(Math.round(7.444)); //7
+
+//random value
+console.log(Math.random()); //0 to 1 random value generated
+
+
+//String operations
+
+
+let str = "Ankush";
+
+//uppercase
+console.log(str.toUpperCase());
+
+
+//lowercase
+console.log(str.toLowerCase());
+
+
+//indexof
+console.log(str.indexOf("u")); // 3
+
+
+//lastindexof
+console.log(str.lastIndexOf("A")); // 0 last return
+
+
+//chartAt
+console.log(str.charAt(4)); // s
+
+
+//split all value
+console.log(str.split('')); // ['A','n','k','u','s','h']
+
+
+//concate
+console.log(str.concat(" Dhavale"));  // Ankush Dhavale
+
+
+//uppercase
+console.log(str.slice(1,3)); // return 1 index to 3-1 index // nk
+
+
+
