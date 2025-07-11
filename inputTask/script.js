6d36549f1c518d9bb2a88f6a1e80361bef8bdb02
Ankush Kailas Dhavale <dhavaleankush7@gmail.com>
2025-07-11 23:17:14 +0530

    js

--- inputTask/script.js
+++ inputTask/script.js
@@ -1,8 +1,26 @@
+const titleInp = document.querySelector(".title");
+const imgInp = document.querySelector(".img");
+const addBtn = document.querySelector("button");
 const mainDiv = document.querySelector("#main");
-const box = document.querySelector(".box");
-const body = document.body;
 
-document.addEventListener("mousemove", () => {
-    box.style.transition = translateX(100);
+addBtn.addEventListener("click", () => {
+	if (imgInp.value === "" && titleInp.value === "") {
+		alert("Please put the Input Value");
+		return;
+	}
+	const para = document.createElement("p");
+	const img = document.createElement("img");
+	const div = document.createElement("div");
+	img.src = `${imgInp.value}`;
+	console.log(imgInp.value);
+	div.classList = "sub-div";
+	img.alt = "Please can Add correct img Url";
+	para.classList = "para";
+	para.textContent = titleInp.value;
+	mainDiv.appendChild(para);
+	div.append(para, img);
+	mainDiv.appendChild(div);
+	imgInp.value = "";
+	titleInp.value = "";
 })
 
